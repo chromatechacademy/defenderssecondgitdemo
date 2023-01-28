d66941022955f59db0f8546c287e82973d95fbcd
Samuel Coreas <samuelcoreas@macbook-pro.myfiosgateway.com>
2023-01-27 20:39:19 -0500

    Created

---
+++ src/main/java/com/gitdemotwo/SammyMain.java
@@ -0,0 +1,12 @@
+package com.gitdemotwo;
+
+public class SammyMain {
+   public static void main(String[] args){
+    SammyMain obj = new SammyMain();
+    obj.print("sammy");
+   }
+
+private void print(String name) {
+System.out.println(name);
+ } 
+}
