c08a7b9c3076df7721985d744b2e34bf3bb2ac69
Samuel Coreas <samuelcoreas@macbook-pro.myfiosgateway.com>
2023-01-27 21:27:11 -0500

    sending my test sammy#

---
+++ src/test/java/com/gitdemotwo/SamuelMain.java
@@ -0,0 +1,12 @@
+package com.gitdemotwo;
+
+public class SamuelMain {
+    public static void main(String[] args){
+        SamuelMain obj = new SamuelMain();
+        obj.sammyName("Sammy");
+    }
+
+    public void sammyName(String name){
+        System.out.println(name);;
+    }
+}
