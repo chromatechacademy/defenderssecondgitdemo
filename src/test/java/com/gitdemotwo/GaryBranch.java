b96c97f3ec894d58dc1906da74dd7284401cd216
Gary Eng <geng20197@gmail.com>
2023-01-27 20:56:49 -0500

    Ctrl Shift F

--- src/test/java/com/gitdemotwo/GaryBranch.java
+++ src/test/java/com/gitdemotwo/GaryBranch.java
@@ -1,17 +1,15 @@
 package com.gitdemotwo;
 
 public class GaryBranch {
-        public static void main(String[] args) {
-            GaryBranch obj = new GaryBranch();
-            obj.print("Gary");
-        }
+    public static void main(String[] args) {
+        GaryBranch obj = new GaryBranch();
+        obj.print("Gary");
+    }
 
-
-
-    public void print(String name){
+    public void print(String name) {
 
         System.out.println(name);
 
     }
-    
+
 }
