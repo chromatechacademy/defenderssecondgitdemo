1905b364832ed6d166ae16a7fa1c8465f4fc8765
SamHardy1013 <semir.im.grey@icloud.com>
2023-01-27 21:34:05 -0500

    fixed error AbuMain

--- src/test/java/com/gitdemotwo/AbuMain.java
+++ src/test/java/com/gitdemotwo/AbuMain.java
@@ -2,13 +2,12 @@ package com.gitdemotwo;
 
 public class AbuMain {
     public static void main(String[] args) {
-        AbuMain obj = new AbuMain(); 
+        AbuMain obj = new AbuMain();
         obj.print("Abu");
     }
 
     public void print(String name) {
         System.out.println(name);
     }
-    
-}
+
 }
