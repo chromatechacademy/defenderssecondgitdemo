a248f25c3aa3c3433ca9b21ef2775c5b2d21c559
Gary Eng <geng20197@gmail.com>
2023-01-27 20:51:25 -0500

    Created Gary Branch

---
+++ src/test/java/com/gitdemotwo/GaryBranch.java
@@ -0,0 +1,17 @@
+package com.gitdemotwo;
+
+public class GaryBranch {
+        public static void main(String[] args) {
+            GaryBranch obj = new GaryBranch();
+            obj.print("Gary");
+        }
+
+
+
+    public void print(String name){
+
+        System.out.println(name);
+
+    }
+    
+}
