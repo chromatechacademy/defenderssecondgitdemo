ec17a643a43443e73d731a7883fb3cd055bdc575
Toropovv <iliya.toropov@gmail.com>
2023-01-27 20:54:15 -0500

    created IlyaMain

---
+++ src/test/java/com/gitdemotwo/IlyaMain.java
@@ -0,0 +1,17 @@
+package com.gitdemotwo;
+
+public class IlyaMain {
+    public static void main(String[] args) {
+    
+        IlyaMain obj = new IlyaMain();
+        obj.print("Ilya");
+        
+        
+        }
+        
+        public void print(String qwerty){
+            System.out.println(qwerty);
+        }
+        
+        }
+
