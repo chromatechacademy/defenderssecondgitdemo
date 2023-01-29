dcad24c0aff8675067a32bbe30e77ecb92a473cd
Ekanayake Gangoda <kushangurugamage@Kushans-iMac.local>
2023-01-29 16:02:43 -0500

    Created Umesha main

---
+++ src/test/java/com/gitdemotwo/UmeshaMain.java
@@ -0,0 +1,13 @@
+package com.gitdemotwo;
+
+
+public class UmeshaMain {
+    public static void main(String []args){
+        UmeshaMain obj = new UmeshaMain();
+
+    }
+    public void print(String name)
+{
+    System.out.println(name);
+}    
+}
