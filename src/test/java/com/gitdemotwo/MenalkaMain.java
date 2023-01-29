62fe89779b2f081f81913410ae59b9831c9254d7
menalka weerasinghe <menalkaweerasinghe@menalkas-iMac.local>
2023-01-29 16:00:59 -0500

    created Menalka main

---
+++ src/test/java/com/gitdemotwo/MenalkaMain.java
@@ -0,0 +1,16 @@
+package com.gitdemotwo;
+
+public class MenalkaMain {
+    public static void main(String[] args) {
+        MenalkaMain obj = new MenalkaMain();
+        obj.print("Menalka");
+        
+
+
+        
+    }
+    public static void print (String name){
+
+        System.out.println(name);
+    }
+}
