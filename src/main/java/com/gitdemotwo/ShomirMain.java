4dc8c5fe365cb554e1260cccb3dcc825dd97d1aa
Shomir Uddin <shomiruddin@shomirs-mbp.myfiosgateway.com>
2023-02-01 21:40:07 -0500

    printing shomir in shomir main

--- src/main/java/com/gitdemotwo/ShomirMain.java
+++ src/main/java/com/gitdemotwo/ShomirMain.java
@@ -4,12 +4,8 @@ public class ShomirMain {
 
     public static void main(String[] args) {
         ShomirMain obj = new ShomirMain(); 
-        obj.print("Kevin");
-        obj.print("Hello");
-        obj.print("Hello Hi");
-        
-        ShomirMain obj2 = new ShomirMain();
-        obj2.print("This is object 2");
+        obj.print("Shomir");
+    
     }
 
     public void print(String name) {
